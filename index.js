1679c139462683478ea1c4af130b1e7b20c31b94
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-11-19 13:14:05 +0200

    Update the package to have multiple functions

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = (str) => {
-    return str.toUpperCase();
+function shout(str) {
+    return str.toUpperCase()
+};
+
+function whisper(str) {
+    return str.toLowerCase()
+};
+
+function trimTheString(str) {
+    return str.trim()
 }
+
+function wrapWithLove(str) {
+    return `❤️${str}❤️`
+}
+
+module.exports = { shout, whisper, trimTheString, wrapWithLove };
